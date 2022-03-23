2c6a4228bca9025dc838dc2a38676d3e239336cf
hfeuerstein <hendrik.feuerstein98@gmail.com>
2022-03-23 09:57:58 +0100

    maybe final script

--- pipeline_script.sh
+++ pipeline_script.sh
@@ -17,28 +17,25 @@
 # INPUT: two fastq files (r1 and r2)
 ##############################################
 
-
 function preprocess {
-	# Choose prefixes for alignment output files:
-	read -p "Choose file prefixes:" answer
-	mkdir fastqc discarded_seq alignment_results counts
+	# Extracts filename from path and uses the filename until _r1* or _r2* for output file names 
+	NAME=$(basename "$1")
+	PREFIX=${NAME%%_r*}
+
+	mkdir fastqc trimmed_fastq discarded_fastq alignment_results counts
 	
 	#1) Quality Control with FastQC
 	fastqc -o fastqc $@
 
 	#2) Adapter trimming with trimmomatic: 
 	trimmomatic PE $1 $2 \
-		${answer}_r1_trim ./discarded_seq/${answer}_r1_un.trim \
-		${answer}_r2_trim ./discarded_seq/${answer}_r2_un.trim \
+		./trimmed_fastq/${PREFIX}_r1_trim.fastq ./discarded_fastq/${PREFIX}_r1_un.trim.fastq \
+		./trimmed_fastq/${PREFIX}_r2_trim.fastq ./discarded_fastq/${PREFIX}_r2_un.trim.fastq \
 		ILLUMINACLIP:./required/NexteraPE-PE.fa:2:40:15
 
 	#3) Alignment with STAR
-	STAR --runThreadN 6 --genomeDir ./required/index --readFilesIn *r1_trim *r2_trim --outFileNamePrefix ./alignment_results/${answer}_ --outSAMtype BAM SortedByCoordinate
+	STAR --runThreadN 6 --genomeDir ./required/index --readFilesIn ./trimmed_fastq/*r1_trim.fastq ./trimmed_fastq/*r2_trim.fastq --outFileNamePrefix ./alignment_results/${PREFIX}_ --outSAMtype BAM SortedByCoordinate
 
 	#4) Quantification with HTSeq:
 	htseq-count --format=bam --order=pos --stranded=no ./alignment_results/*out.bam ./required/*annotation.gtf > ./counts/count_table.tab 
 }
-
-
-
-
