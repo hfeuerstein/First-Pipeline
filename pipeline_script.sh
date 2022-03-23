3239ed541d8eed872abdd006f151653ccea3d25f
hfeuerstein <hendrik.feuerstein98@gmail.com>
2022-03-23 14:09:02 +0100

    final script

--- pipeline_script.sh
+++ pipeline_script.sh
@@ -3,8 +3,11 @@
 ###############################################
 # Function "preprocess": basic bash script for preprocessing Fastq-files incl: 
 # 	- quality control (FastQC)
-#	- adapter trimming (trimmomatic) for NexteraPE-PE
+#	- adapter trimming (trimmomatic) for NexteraPE-PE (default) 
+#           - for additional base quality trimming e.g.: SLINDINGWINDOW:4:20 (sliding window size 4 that removes bases below phred=20)
+#           - to remove reads shorter than e.g. 25bp: MINLEN:25 
 #	- alignment (STAR)
+#	- bam file indexing for HTSeq (SamTools)
 #	- quantification (HTSeq)
 #
 # REQUIREMENTS:
@@ -13,6 +16,8 @@
 #		- NexteraPE-PE.fa: avaibale via  ~/miniconda3/pkgs/trimmomatic-0.39-hdfd78af_2/share/trimmomatic-0.39-2/adapters/NexteraPE-PE.fa
 #		- GTF file for annotation: with name *annotation.gtf
 #		- folder "index" with indexed genome/subset (from STAR)
+#
+# NOTE: samtools and htseq have conflicting issues (recommended packages: python=3.7, samtools=1.9)
 #
 # INPUT: two fastq files (r1 and r2)
 ##############################################
@@ -34,8 +39,11 @@ function preprocess {
 		ILLUMINACLIP:./required/NexteraPE-PE.fa:2:40:15
 
 	#3) Alignment with STAR
-	STAR --runThreadN 6 --genomeDir ./required/index --readFilesIn ./trimmed_fastq/*r1_trim.fastq ./trimmed_fastq/*r2_trim.fastq --outFileNamePrefix ./alignment_results/${PREFIX}_ --outSAMtype BAM SortedByCoordinate
+	STAR --runThreadN 4 --genomeDir ./required/index --readFilesIn ./trimmed_fastq/*r1_trim.fastq ./trimmed_fastq/*r2_trim.fastq --outFileNamePrefix ./alignment_results/${PREFIX}_ --outSAMtype BAM SortedByCoordinate
 
-	#4) Quantification with HTSeq:
+	#4) Bam file Indexing for HTSeq 
+	samtools index ./alignment_results/*.out.bam 
+
+	#5) Quantification with HTSeq:
 	htseq-count --format=bam --order=pos --stranded=no ./alignment_results/*out.bam ./required/*annotation.gtf > ./counts/count_table.tab 
 }
